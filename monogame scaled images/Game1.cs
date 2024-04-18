e66268d50d5a60c1416283d2e26600d838b9730a
Domenic Maniacco <dmaniacco3291@gmail.com>
2024-04-18 13:05:30 -0400

    Done :)

--- monogame scaled images/Game1.cs
+++ monogame scaled images/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Globalization;
 
 namespace monogame_scaled_images
 {
@@ -8,6 +9,9 @@ namespace monogame_scaled_images
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D rectangle, circle;
+
 
         public Game1()
         {
@@ -19,6 +23,10 @@ namespace monogame_scaled_images
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 640;
+            _graphics.ApplyChanges();
+
 
             base.Initialize();
         }
@@ -26,6 +34,9 @@ namespace monogame_scaled_images
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            rectangle = Content.Load<Texture2D>("rectangle");
+            circle = Content.Load<Texture2D>("circle");
 
             // TODO: use this.Content to load your game content here
         }
@@ -35,6 +46,10 @@ namespace monogame_scaled_images
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
+
+
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -43,6 +58,40 @@ namespace monogame_scaled_images
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            int rectSize = 80; // Size of each rectangle
+            int rows = 8; // Number of rows
+            int cols = 8; // Number of columns
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int xPos = j * rectSize; // Calculate x position based on column
+                    int yPos = i * rectSize; // Calculate y position based on row
+
+                    Rectangle rect = new Rectangle(xPos, yPos, rectSize, rectSize);
+                    if (j%2 == 0 && i%2 == 0)
+                    {
+                        _spriteBatch.Draw(rectangle, rect, Color.Black);
+                    }
+                    else if (j%2 == 1 && i%2 == 1)
+                    {
+                        _spriteBatch.Draw(rectangle, rect, Color.Black);
+                    }
+                    else
+                    {
+                        _spriteBatch.Draw(rectangle, rect, Color.White);
+
+                    }
+
+
+                }
+            }
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
